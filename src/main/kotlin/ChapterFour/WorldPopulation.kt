2e8e9d06f426d68467e3640262167730eef9aa65
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-28 10:27:30 +0100

    restructed code

--- src/main/kotlin/ChapterFour/WorldPopulation.kt
+++ src/main/kotlin/ChapterFour/WorldPopulation.kt
@@ -6,28 +6,29 @@ class WorldPopulation {
     private var percentageIncrease : Double = 1.0103
     private var previousWorldPopulation : Double = 0.0
 
-    fun growthIncrease(years: Int) : String{
+    private fun growthIncrease(years: Int) : String{
         previousWorldPopulation = worldPopulation
-        worldPopulation = worldPopulation * percentageIncrease.pow(years)
+        worldPopulation *= percentageIncrease.pow(years)
         return (String.format("%.4fBillion", worldPopulation))
     }
 
-    fun differenceInPopulation() : String{
-        var difference = worldPopulation - previousWorldPopulation
+    private fun differenceInPopulation() : String{
+        val difference = worldPopulation - previousWorldPopulation
         return (String.format("%.4fBillion", difference))
     }
 
     fun displayGrowthTable(numberOfYears : Int) : String{
-        var result = String.format("%-40d%-40s%-30s", numberOfYears, growthIncrease(numberOfYears), differenceInPopulation());
-        return (result);
+        val result = String.format("%-40d%-40s%-30s", numberOfYears, growthIncrease(numberOfYears),
+            differenceInPopulation())
+        return (result)
     }
 
 }
 fun main() {
     val calculator = WorldPopulation()
-    var result = String.format("%-30s%-40s%-30s", "Year", "World Population after Year", "Difference in Population " +
-            "After Year");
-    println(result);
+    val result = String.format("%-30s%-40s%-30s", "Year", "World Population after Year", "Difference in Population " +
+            "After Year")
+    println(result)
     for (i in 1..75){
         println(calculator.displayGrowthTable(i))
     }
